e3772ade5ff9458b75f355dd858be173aadfae17
huzi <x18g032@chiba-fjb.ac.jp>
2019-09-03 15:06:57 +0900

    アニメーション

---
+++ Assets/unityちゃんアニメ関係/Atack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator animator;
+
+    public Collider rightToeBase;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+        rightToeBase = GameObject.Find("Character1_RightToeBase").GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //Sを押すとHikick
+        if (Input.GetKeyDown(KeyCode.F)){
+            animator.SetBool("Kick", true);
+ 
+            //右足コライダーをオンにする
+            rightToeBase.enabled = true;
+ 
+            //一定時間後にコライダーの機能をオフにする
+            Invoke("ColliderReset", 1.5f);
+        }
+    }
+
+    private void ColliderReset(){
+        // handCollider.enabled = false;
+        rightToeBase.enabled = false;
+    }
+    void Hit(){
+
+    }
+}
---
+++ Assets/unityちゃんアニメ関係/AtackHit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtackHit : MonoBehaviour{    
+    //オブジェクトと接触した瞬間に呼び出される
+    void OnTriggerEnter(Collider other){
+ 
+        //攻撃した相手がEnemyの場合
+        if(other.CompareTag("Enemy")){
+            
+            Destroy(other.gameObject);
+ 
+        }
+    }
+}
