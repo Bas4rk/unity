f0c134de9f1a91203d55f3c62a1b89f2a82c89fd
hasegawa <username@example.com>
2019-11-28 15:18:45 +0900

    shord

---
+++ Assets/unityちゃんアニメ関係/Shord Folder/ShordGetScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShordGetScript : MonoBehaviour
+{
+// Start is called before the first frame update
+const float TARAGET_SEARCH=2f; //索敵範囲
+public GameObject TargetObject; ///目標位置
+GameObject o;
+void Start()
+{
+    o = GameObject.Find("Character1_LeftHand");
+}
+// Update is called once per frame
+void Update(){
+float dis = Vector3.Distance(this.transform.position,TargetObject.transform.position);
+if(dis<TARAGET_SEARCH){
+GameObject obj = (GameObject)Resources.Load("shord");
+Instantiate(obj,o.transform.position,o.transform.rotation);
+}
+}
+}
